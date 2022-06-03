411f85f9798efaf69c7b2420e209538645eb17ee
Lewis Sutton <alloutapparel@outlook.com>
2022-06-03 16:12:22 +0100

    add section

--- assets/product-page.js
+++
@@ -1,8 +0,0 @@
-function showSection(element) {
-    var x = document.getElementById(element);
-    if (x.style.display != 'block') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
