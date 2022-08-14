01003b624e80d3f29b7e92fd310306e0713af7cc
Lewis Sutton <alloutapparel@outlook.com>
2022-08-14 12:45:16 +0100

    js test 2

--- assets/legal.js
+++ assets/legal.js
@@ -1,5 +1,18 @@
-document.querySelectorAll('.legal-menu-item').forEach(a => {
+ /* document.querySelectorAll('.legal-menu-item').forEach(a => {
     a.addEventListener('click', () => {
         a.classList.toggle('legal-menu-item--active');
+    })
+}) */
+
+document.getElementById('#tandclink').forEach(a => {
+    a.addEventListener('click', () => {
+        const container = document.getElementById('#tandc-container');
+        a.classList.toggle('legal-menu-item--active');
+
+        if (a.classList.contains('legal-menu-item--active')) {
+            container.style.display = "block";
+        } else {
+            container.style.display = "none";
+        }
     })
 })
