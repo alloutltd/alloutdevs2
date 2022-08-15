4f58976d52cb437edd1341a84ab7c9b0558d0584
Lewis Sutton <alloutapparel@outlook.com>
2022-08-15 19:50:59 +0100

    again

--- assets/faq.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll('a');
-
-buttons.forEach( a =>{
-    a.addEventListener('click',()=>{
-        const icon = a.children[1];
-
-        icon.classList.toggle('rotate');
-    })
-} )
