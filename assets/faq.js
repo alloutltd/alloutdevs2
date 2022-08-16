e6480f78d53584b2b1d8bf17904619b7bc95983e
Lewis Sutton <alloutapparel@outlook.com>
2022-08-16 20:36:47 +0100

    js

--- assets/faq.js
+++ assets/faq.js
@@ -7,3 +7,35 @@ function keepLocation(oldOffset) {
   }
   setTimeout('window.scrollTo(0,st)',10);
 }
+
+window.onload = function() {
+    document.getElementById("deliverylink").classList.add("active");
+    document.getElementById("delivery-section").classList.add("active");
+}
+
+navbar = document.querySelector(".nav-links").querySelectorAll("a");
+console.log(navbar);
+
+navbar.forEach(element => {
+    element.addEventListener("click", function(){
+
+        var delivery = document.getElementById("deliverylink");
+        var refund = document.getElementById("refundlink");
+
+        navbar.forEach(nav=>nav.classList.remove("active"))
+
+        this.classList.add("active");
+
+        if (delivery.classList.contains("active")) {
+            document.getElementById("delivery-section").classList.add("active")
+        } else {
+            document.getElementById("delivery-section").classList.remove("active")
+        }
+        if (refund.classList.contains("active")){
+            document.getElementById("refund-section").classList.add("active")
+        } else {
+            document.getElementById("refund-section").classList.remove("active")
+        }
+    })
+})
+
